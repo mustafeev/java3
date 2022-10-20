5d23188487d6f9c82b42448c18b252dd6abbad62
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-10-20 21:39:55 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 100;
+        int client = 1100;
+        System.out.println("Бонус:" + (account + client + account / 9));
+
+
+    }
+}
