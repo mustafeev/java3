0de10fda3d903f6c01d9bee6b5b28f42dcd96a85
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-11-05 18:42:01 +0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,15 @@ public class Main {
 
         int account = 100;
         int client = 1100;
-        System.out.println("Бонус:" + (account + client + client / 100));
-
-
+        // System.out.println("Бонус:" + (account + client + client / 100));
+        System.out.println("Счет:" + (account + client + client / 100));
+        int bonus = client;
+        if (bonus <= 1000) {
+            //bonus = (client/client);
+            System.out.println("Не достаточно денег для начисления бонусов:");
+        } else {
+            bonus = (client/100);
+            System.out.println("Бонусы:" + bonus);
+        }
     }
 }
