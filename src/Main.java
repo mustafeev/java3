692869cc2442d76be4a45cfd3d743ad266768a1e
Vadim Mustafeev <ya.mystafeev@yandex.ru>
2022-11-09 22:50:10 +0400

    maybefinall

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@ public class Main {
 
         int account = 100;
         int client = 1100;
-        // System.out.println("Бонус:" + (account + client + client / 100));
-        System.out.println("Счет:" + (account + client + client / 100));
+        if (client < 1000) {
+            System.out.println("Счет:" + (account + client));
+        } else {
+            System.out.println("Счет:" + (account + client + client / 100));
+        }
         int bonus = client;
-        if (bonus <= 1000) {
+        if (bonus < 1000) {
             //bonus = (client/client);
             System.out.println("Не достаточно денег для начисления бонусов:");
         } else {
-            bonus = (client/100);
+           bonus = (client / 100);
             System.out.println("Бонусы:" + bonus);
         }
     }
